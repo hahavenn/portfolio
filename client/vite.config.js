69a293f6a992c6d900fa1e9e945e343fb834f84a
hahavenn <hahavenn@gmail.com>
2023-10-29 14:34:22 +0300

    localhost -> 127.0.0.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import * as path from "path";
 export default defineConfig({
 	server: {
 		port: 4000,
-		host: "localhost",
+		host: "127.0.0.1",
 	},
 	resolve: {
 		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
